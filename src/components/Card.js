6ff15a1493e1160318c62ca9412a74ea6e0854d7
Natalia Aronson <nataronso@gmail.com>
2021-08-29 18:19:56 +0200

    changing card

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,21 +1,30 @@
-import React from 'react' 
-import Cardstyle from '.components/Cardstyle';
 
-function Card(props)
- { return ( 
-<div className="card">
-<div className="card-header">
-<h2 className="card-name">{props.name}</h2>
-</div>
- <div className="card-body">
- <p className="card-price">рублей{props.price}/мес</p>
- <p className="card-speed">до{props.speed}Мбит/сек</p>
- </div>
- <div className="card-footer">
- <p className="card-text">{props.text}</p>
-</div>
- </div>
- </
- );
+import React from 'react';
+class Card extends React.Component {
+	render() {
+const { name, price, speed, text } = this.props;
+		return (
+            <div className="card">
+            <div className="card-header">
+            <h2 className="card-name">{name}</h2>
+            </div>
+             <div className="card-body">
+             <p className="card-price">рублей{price}/мес</p>
+             <p className="card-speed">до{speed}Мбит/сек</p>
+             </div>
+             <div className="card-footer">
+             <p className="card-text">{text}</p>
+	
+				</div>
+			</div>
+		);
+	}
 }
-Export default Card;
+
+export default Card;
+
+
+
+
+
+
