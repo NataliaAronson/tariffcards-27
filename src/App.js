8ac84692d5b25dd579d6c9e32ae4ee6469b16bfd
Natalia Aronson <nataronso@gmail.com>
2021-08-29 18:26:41 +0200

    putting style in app. css

--- src/App.js
+++ src/App.js
@@ -1,30 +1,31 @@
 
-import React from 'react' 
- import Card from '.components/Card';
- import Cardstyle from '.tariffcards/components/Cardstyle.css';
- class CardList extends React.Component { 
-render() {
- return (
- <React.Fragment>
- <Card style={{backgroundColor:#61dafb}}
+import React from 'react';
+import './App.css';
+import Card from './components/Card';
+class CardList extends React.Component {
+  render() {
+      return (
+        <React.Fragment>
+
+ <Card 
 name="Безлимитный 300" 
 price={300}
 speed={10}
 text="Обьем включённого трафика не ограничен" 
  />
- <Card style={{}}
+ <Card 
 name="Безлимитный 450" 
 price={450}
 speed={50}
 text="Обьем включённого трафика не ограничен" 
  />
-  <Card style={{}}
+  <Card 
 name="Безлимитный 500" 
 price={550}
 speed={100}
 text="Обьем включённого трафика не ограничен" 
  />
-  <Card style={{}}
+  <Card 
 name="Безлимитный 1000" 
 price={1000}
 speed={200}
