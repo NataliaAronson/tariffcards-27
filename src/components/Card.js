fcdf3c2eb3ced11b7cb436848bb512fc3b023232
Natalia Aronson <nataronso@gmail.com>
2021-09-01 18:35:36 +0200

    looking for correct place  proprs color

--- src/components/Card.js
+++ src/components/Card.js
@@ -4,9 +4,9 @@ import './Cardstyle.css';
 
 class Card extends React.Component {
 	render() {
-const { name, price, speed, text, isSelected } = this.props;
+const { name, price, speed, text, style} = this.props;
 		return (
-            <div className={"card"+(isSelected?"selected":"")}>
+            <div className="card"{style}>
             <div className="card-header">
             <h2 className="card-name">{name}</h2>
             </div>
