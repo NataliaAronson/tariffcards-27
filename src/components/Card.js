250cb87c21570144959f883958486060d89b6284
Natalia Aronson <nataronso@gmail.com>
2021-09-01 19:59:31 +0200

    looking for the border

--- src/components/Card.js
+++ src/components/Card.js
@@ -11,8 +11,8 @@ const { name, price, speed, text} = this.props;
             <h2 className="card-name">{name}</h2>
             </div>
              <div className="card-body">
-             <p className="card-price">рублей{price}/мес</p>
-             <p className="card-speed">до{speed}Мбит/сек</p>
+             <p className="card-price">рублей {price} /мес</p>
+             <p className="card-speed">до {speed} Мбит/сек</p>
              </div>
              <div className="card-footer">
              <p className="card-text">{text}</p>
